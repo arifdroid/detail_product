cf6ffda4ce2aa9ebd2d9e08226425979a3a61cd3
arif hanif <arifdroiddeveloper@gmail.com>
2019-09-08 03:03:33 -0700

    work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // https://stackoverflow.com/questions/49132220/iterate-through-an-html-table-using-cheerio
 
+const ProsesData = require('./components/Proses_single_data'); 
 
 var request = require('request-promise');
 var cheerio = require('cheerio');
@@ -12,7 +13,7 @@ const url2 = 'https://www.skincarisma.com/products/cosrx/salicylic-acid-daily-ge
 
 (async () => {
 
-    const response2 = await request(url);
+    //const response2 = await request(url);
     //const response3 = await request(url);
 
     const response3 = await request(url2);
@@ -20,15 +21,17 @@ const url2 = 'https://www.skincarisma.com/products/cosrx/salicylic-acid-daily-ge
     let $ = cheerio.load(response3);
 
 
-    var splitthem = response2.match(/semibold dotted-underline ml-1">(.*?)<\/span>/g)
+    //var splitthem = response2.match(/semibold dotted-underline ml-1">(.*?)<\/span>/g)
 
     //console.log('split them ', splitthem)
 
-    var splitthem_2 = response2.match(/class="fa(.*?)font-095"><\/i>/g)
+    //var splitthem_2 = response2.match(/class="fa(.*?)font-095"><\/i>/g)
 
-    const lookk = $('table[class="table table-sm mt-4 ingredients-table"]>tbody>tr>td[class="align-middle"]');
+    //const lookk = $('table[class="table table-sm mt-4 ingredients-table"]>tbody>tr>td[class="align-middle"]');
 
     const table_ingredient = $('table[class="table table-sm mt-4 ingredients-table"]>tbody>tr');
+
+    _kiv = () =>{
 
     //var ingredientTest = response3.match(/<td class="align-middle">(.*?)<br\/>\n<small>/g)
 
@@ -38,183 +41,253 @@ const url2 = 'https://www.skincarisma.com/products/cosrx/salicylic-acid-daily-ge
     ///////////
 
 
-    splitthem.forEach((element, i) => {
+    // splitthem.forEach((element, i) => {
 
-        var rem = element.replace("semibold dotted-underline ml-1\">", "").replace("</span>", "")
+    //     var rem = element.replace("semibold dotted-underline ml-1\">", "").replace("</span>", "")
 
-        console.log('here >  ' + i, rem)
+    //     console.log('here >  ' + i, rem)
 
-    });
+    // });
 
-    console.log('\n\n')
+    // console.log('\n\n')
 
 
-    console.log('\n\n')
+    // console.log('\n\n')
 
 
     //console.log('nigga >> ', lookk.text())
 
-    var ingredient = []
-    var i = 0;
+    // var ingredient = []
+    // var i = 0;
 
 
 
-    lookk.toArray().map((item) => {
+    // lookk.toArray().map((item) => {
 
-        var check_0 = i % 2;
+    //     var check_0 = i % 2;
 
-        if (check_0 == 0) {
+    //     if (check_0 == 0) {
 
-            ingredient.push($(item).text().trim())
+    //         ingredient.push($(item).text().trim())
 
 
 
-        }
+    //     }
 
-        i++;
+    //     i++;
 
-    });
+    // });
 
     // console.log('\n\ningred List >> ', ingredient.length+'\n\n')
+    }
+
+    ///////////////////////// real work 
+
 
     var stringingred_2 = '';
+
+    var stringIngred_Array =[];
 
     console.log('\n\n')
 
 
     table_ingredient.each((i, element) => {
 
+        var stringingred_3 = $(element).children('.align-middle').text();
 
         if (i == 22) {
             console.log('table >> ', $(element).children('.align-middle').text() + '\n')
 
-            stringingred_2 = $(element).children('.align-middle').text();
+           stringingred_2 = $(element).children('.align-middle').text();
 
         }
+
+        stringIngred_Array.push(stringingred_3);
 
     })
 
 
-    console.log('stringingred_2 >> ', stringingred_2.trim())
+    //console.log('stringingred_2 >> ', stringingred_2.trim())
 
-    var finall = '';
+    console.log('\n\n array LOOKUP > ',stringIngred_Array)
 
-    var counter = 0;
+    console.log('\n\n')
 
-    while (stringingred_2.includes("\n")) {
+    ////
 
-        stringingred_2 = stringingred_2.replace("\n", "");
+    var stringIngred_Array_2 =[];
+
+    //remove \n in every array first 
+    stringIngred_Array.forEach((el,i)=>{
+
+        //var stringhere = '';
+
+        while(el.includes("\n")){
+
+            el = el.replace("\n","");
+
+        }
+
+        //stringhere = el;
+
+        stringIngred_Array_2.push(el)
+
+    }) //finish remove \n
+
+    // NOW OBJECTIFY
+
+    var final_Object = [];
+
+    var proses_data = new ProsesData;
+
+
+    stringIngred_Array_2.forEach(el=>{
+
+        var objectGot = [];
+
+        objectGot = proses_data.fungsi_prosesData(el); 
+
+
+        final_Object.push(objectGot);
+        
+
+    });
+
+
+    console.log('\n\n\n ..........................')
+
+
+    console.log('FINAL >> LENGTH :'+final_Object.length)
+
+    console.log('\n')
+
+    for(var i=0; i<6;i++){
+
+        console.log('\ni :'+i+ ' ,, object is = '+ JSON.stringify(final_Object[i])+'\n')
 
     }
 
-    //console.log("\n\n final >> ", stringingred_2.trim())
 
-    console.log('\n\nFINALLL >> , ')
+    ////////////////
 
-    var processsplit = stringingred_2;
+    // var finall = '';
 
-    var pattern = processsplit.match(/\s\s[a-z|A-Z|0-9](.*?)\s\s/gi);
+    // var counter = 0;
 
-    var finalize_ingredient = [];
+    // while (stringingred_2.includes("\n")) {
 
-    var ewg = 0;
+    //     stringingred_2 = stringingred_2.replace("\n", "");
 
-    var cir = '';
+    // }
 
-    var ingredient_is = '';
+    // //console.log("\n\n final >> ", stringingred_2.trim())
 
-    var note = [];
-    //var 
+    // console.log('\n\nFINALLL >> , ')
 
-    console.log('\n\n YOO \n\n')
+    // var processsplit = stringingred_2;
 
-    pattern.forEach((el,i)=>{
+    // var pattern = processsplit.match(/\s\s[a-z|A-Z|0-9](.*?)\s\s/gi);
 
-        console.log(i+' contain = ', el)
+    // var finalize_ingredient = [];
 
-    })
+    // var ewg = 0;
 
-    console.log('\n\n BEFORE\n\n')
+    // var cir = '';
 
-    pattern.forEach((el, i) => {
+    // var ingredient_is = '';
 
-        var words = el.trim();
-        var testHere = words.charAt(1); // if not found, or single letter  == ''
+    // var note = [];
+    // //var 
 
-        if (i == 0) {
-            if (testHere == '') {
+    // console.log('\n\n YOO \n\n')
 
-                if(words.match(/[\d]/i)){
-                    ewg = words;
+    // pattern.forEach((el,i)=>{
 
-                }else{ //ewg not recorded
-                    cir = words
-                }
+    //     console.log(i+' contain = ', el)
 
-           } else { //b
+    // })
 
-                console.log('HERE NIGG')
+    // console.log('\n\n BEFORE\n\n')
 
-                ingredient_is = words;
+    // pattern.forEach((el, i) => {
 
-            }
-        }else if(i==1){
+    //     var words = el.trim();
+    //     var testHere = words.charAt(1); // if not found, or single letter  == ''
 
-            if(testHere==''){ // means that, 
+    //     if (i == 0) {
+    //         if (testHere == '') {
 
-                cir=words;  
-            }else{
+    //             if(words.match(/[\d]/i)){
+    //                 ewg = words;
 
-                if(ingredient_is==''){
-                     ingredient_is = words;
-                }else{
+    //             }else{ //ewg not recorded
+    //                 cir = words
+    //             }
 
+    //        } else { //b
 
-                    note.push(words)
-                }
+    //             console.log('HERE NIGG')
 
-            }
+    //             ingredient_is = words;
 
+    //         }
+    //     }else if(i==1){
 
+    //         if(testHere==''){ // means that, 
 
-        }else if(i==2){
+    //             cir=words;  
+    //         }else{
 
-              if(cir!='' && ewg!=0){
-
-                if(ingredient_is==''){
-
-                    ingredient_is=words
-                }else{
-
-                    note.push(words)
-                }
-
-              }else{
-
-                note.push(words);
-              }      
+    //             if(ingredient_is==''){
+    //                  ingredient_is = words;
+    //             }else{
 
 
-        }else{
+    //                 note.push(words)
+    //             }
 
-            note.push(words);
-        }
-
-        if((i+1)==pattern.length){
-
-            finalize_ingredient.push({"ewg":ewg, "cir":cir, "ingredient":ingredient_is,"note":note})
-
-        }
+    //         }
 
 
 
+    //     }else if(i==2){
 
-    })
+    //           if(cir!='' && ewg!=0){
 
-    console.log('\n\n\n\n\n\n ')
-    console.log('>>>>>>>>>>>>>>>>>>>>>')
+    //             if(ingredient_is==''){
 
-    console.log('FINALL >>' + JSON.stringify(finalize_ingredient))
+    //                 ingredient_is=words
+    //             }else{
+
+    //                 note.push(words)
+    //             }
+
+    //           }else{
+
+    //             note.push(words);
+    //           }      
+
+
+    //     }else{
+
+    //         note.push(words);
+    //     }
+
+    //     if((i+1)==pattern.length){
+
+    //         finalize_ingredient.push({"ewg":ewg, "cir":cir, "ingredient":ingredient_is,"note":note})
+
+    //     }
+
+
+
+
+    // })
+
+    // console.log('\n\n\n\n\n\n ')
+    // console.log('>>>>>>>>>>>>>>>>>>>>>')
+
+    //console.log('FINALL >>' + JSON.stringify(finalize_ingredient))
 
 
 
